f4b4d735cd5d503372875b7636f0a5b4c99b006a
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-10-05 15:28:00 -0700

    create db and create db sql

---
+++ db/create_beers.sql
@@ -0,0 +1,35 @@
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('Pure Project','https://i.postimg.cc/sg6G7HR7/images.png','Year Round Brew List: Tropical Mist - Blonde Ale, Fernprost - Lager, Pure West - IPA, Subconcious Mind - IPA, Widened Horizons - IPA, Transdimensional - IPA, Wild Sparrow - IPA, Brave Noise - IPA, The Labyrinth Within - IPA, Everything Is Beautiful - Sour, Roes Red - Batch 6 Sour, Searching For The Sound - Sour, Lief - Batch 6 Sour, Everything Gold May Stay - Sour, Situational Gravity - Stout, Lost Monarch - Stout, Unfathomed Depths - Stout','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('AleSmith','https://i.postimg.cc/G3FBqmCc/Yx-DAd8c-DEWw-TJXVGihi-Gz1f-F.png','Year Round Brew List: San Diego Pale Ale .394, Cloud Stream, Speedway Stout, KickBackRelax, IPA, Nut Brown Ale, Limeberry Twist, Sublime Mexican Lager, Horny Devil','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('California Wild','https://i.postimg.cc/Pq7Zs33T/Unknown.png','Year Round Brew List: Apricots with a View, Berry Allen, Blue on Blue, Blueberry Pie – Pastry Sour Beer, Cabernet Franc Wild Ale – A Funky Wine Grape Sour, Carlsbad Blueberry, Cherry Potter w/ Cacao Nibs, Cherry Potter w/ Vanilla, Chewboocha – Kombucha Sour Ale, Daenerys Tar-Berry-an, Dark Gold Mixed-Fermentation Wild Ale, Echoes of Grenache, Funky Barrel Golden Sour, Golden Ale with a View, Grilled Pineapple Wild Ale, ImPEARial Stormtrooper, Jabba the Hopp w/ Citra Hop, Jabba the Hopp w/ Kohatu Hops, Mai Tai – Kombucha Sour Ale, Mango Fett, May the Force Be With Yuzu, Mead Berry – Collab with Lost Cause Meadery, Mead Berry w/ Vanilla, Mexican Candy, Mr. Tea, Ocean Beach Passion Fruit, Padme Almond-ala, Peach on Peach, Peach on Peach with Pineapple, PerCinnamon – Fuyu Persimmon and Cinnamon, Perfectly Peach, Pickle Rick – Unmistakably Awesome, Pineapple Upside Down Cake – Pastry Sour Beer, Prickly Pear Wild Ale, Princess Peach, Satsuma Mandarin Blueberry Wild Ale, Sour Rangers – It’s Morphin Time!, Sponge Bob Sour Pants – Grilled Pineapple w/ Toasted Coconut, The Darkside – Kombucha Sour Ale, The Mangolorian','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('The Lost Abbey','https://i.postimg.cc/T1fbkqDj/lost-abbey-celtic-cross.jpg','Year Round Brew List: Devotion, Farmhouse, Judgement Day, Lost and Found, Red Barn','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('Stone','https://i.postimg.cc/x1wcx3rk/gargoyle-0.png','Year Round Brew List: Stone IPA, Stone Buenaveza - Salt & Lime Lager, Stone Delicious IPA, Stone Tangerine Express Hazy IPA, Stone Fear.Movie.Lions Hazy Double IPA, Stone Hazy IPA, Stine Neverending Haze IPA, Stone Dayfall Beligian White, Stone Scorpion Bowl IPA, STone Ruination Double IPA 2.0 Sans Filtre','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('Green Flash','https://i.postimg.cc/jjfJtk6Q/GF19-Primary-Logo-RGB.png','Year Round Brew List: Tropical DNA - India Pale Aler, West Coast IPA, Saturhaze IPA, Soul Style IPA','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('Grupo Modelo','https://i.postimg.cc/GmHySpHf/download.jpg','Year Round Brew List: Pacífico, Corona, Modelo Especial, Modelo Light, Modelo Negra, Modelo Reserva, Victoria, Estrella Jalisco, y León','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('Stella Artois','https://i.postimg.cc/050Dz3TV/1200px-Stella-Artois-logo-svg.png','Year Round Brew List: Stella Artois Lager, Stella Artois Solstice Lager and Cidre','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_beer(name, img, bio, verified_beer, created_at)
+  VALUES
+    ('Mother Earth','https://i.postimg.cc/9QMd5pNJ/images.jpg',"Year Round Brew List: Cali Creamin/'- Vanilla Cream Ale, Cali Creamsicle - Orange Vanilla Cream Ale, Tierra Madre - Lager, Hop Diggity - Double IPA, Boo Koo - India Pale Ale, Milk Truck - Latte Stout, and Quit Stalin - BBA Russian Imperial Stout",'t','2021-05-07 18:57:42.517205-07');
